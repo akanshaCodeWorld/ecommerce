61425acb5fbfb3737225e8ffc373655af9161709
Akansha <akansha@hood.live>
2023-11-27 00:00:53 +0530

    react

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import HomePage from './components/Home';
+import CategoryListPage from './components/CategoryList';
+import ProductListPage from './components/Product';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/" exact component={HomePage} />
+        <Route path="/category/:category" component={CategoryListPage} />
+        <Route path="/products" component={ProductListPage} />
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
